997f4a1c55284acf3a52d7fa7ffe1fb4b9e664a6
Joseph Green <joe.green@foxsoftware.net>
2021-05-07 08:03:24 -0600

    Feature/update title (#5)
    * add price to the tab title
    
    * updated investment numbers
    
    Co-authored-by: Joe Green <josepgre@cisco.com>

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -16,9 +16,10 @@ function Dashboard({ price, data }) {
   if (price === "0.00") {
     return <h2>Click the box above</h2>;
   }
-  const initialInvestment = 199.86;
-  const totalValue = (price * .060606).toFixed(2);
-  const openPl = (((totalValue/initialInvestment) - 1) * 100).toFixed(2);
+  const investment = 209.86;
+  const quantity = (price * 0.063564).toFixed(2);
+  const openPl = (((quantity/investment) - 1) * 100).toFixed(2);
+  document.title = 'ETH - $'.concat((price * 1).toFixed(2));
   return (
     <div className="dashboard">
       <img src={crypto} className="crypto-logo" alt="logo" />
@@ -36,7 +37,7 @@ function Dashboard({ price, data }) {
             <td><img src={ethereum} id='ethereum-icon' className='ethereum-icon' alt='ethereum icon'/>Ethereum</td>
             <td>${(price * 1).toFixed(2)}</td>
             <td className={Math.sign(openPl) === -1 ? 'red-text' : 'green-text'}>{openPl}%</td>
-            <td>${totalValue}</td>
+            <td>${quantity}</td>
           </tr>
         </tbody>
       </Table>
